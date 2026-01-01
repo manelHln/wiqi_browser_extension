141e527c6d02f8a4701b003273bd971e80766fb3
Emmanuel HOLONOU <holonouemmanuel0@gmail.com>
2026-01-01 09:35:15 +0100

    commit

--- src/components/LoggedIn.tsx
+++ src/components/LoggedIn.tsx
@@ -1,11 +1,18 @@
 import type { User } from "@supabase/supabase-js"
-import { Home, Search, ShoppingCart, Bell, User as UserIcon } from "lucide-react"
+import {
+  Bell,
+  Home,
+  Search,
+  ShoppingCart,
+  User as UserIcon
+} from "lucide-react"
 import React, { useState } from "react"
+
+import Navbar from "~components/Navbar"
 
 import CategoryPage from "./category-page"
 import EarningsPage from "./earnings-page"
 import HomePage from "./home-page"
-import Navbar from "~components/Navbar"
 import Navigation from "./navigation"
 import NotificationsPage from "./notifications-page"
 import ProfilePage from "./profile-page"
@@ -15,27 +22,35 @@ type Props = {
 }
 
 type navItem = {
-  id: PageType,
-  icon: React.ElementType,
+  id: PageType
+  icon: React.ElementType
   label: string
 }
 
 const navItems = [
-    { id: "home", icon: Home, label: "Home" },
-    { id: "category", icon: Search, label: "Browse" },
-    { id: "earnings", icon: ShoppingCart, label: "Earnings" },
-    { id: "notifications", icon: Bell, label: "Notifications" },
-    { id: "profile", icon: UserIcon, label: "Profile" },
-  ] as const satisfies navItem[]
+  { id: "home", icon: Home, label: "Home" },
+  { id: "category", icon: Search, label: "Browse" },
+  { id: "earnings", icon: ShoppingCart, label: "Earnings" },
+  { id: "notifications", icon: Bell, label: "Notifications" }
+] as const satisfies navItem[]
 
-export type PageType = "home" | "category" | "earnings" | "notifications" | "profile"
+export type PageType =
+  | "home"
+  | "category"
+  | "earnings"
+  | "notifications"
+  | "profile"
 
 export default function LoggedIn({ user }: Props) {
   const [currentPage, setCurrentPage] = useState<PageType>("home")
 
   return (
     <div className="flex-1 overflow-hidden">
-      <Navbar isLoggedIn={true} userEmail={user?.email} setCurrentPage={setCurrentPage} />
+      <Navbar
+        isLoggedIn={true}
+        userEmail={user?.email}
+        setCurrentPage={setCurrentPage}
+      />
       <div className="flex flex-col h-full">
         <div className="flex-1 overflow-y-auto my-16">
           {currentPage === "home" && <HomePage />}
@@ -46,7 +61,11 @@ export default function LoggedIn({ user }: Props) {
         </div>
 
         {/* Bottom Tab Navigation */}
-        <Navigation currentPage={currentPage} setCurrentPage={setCurrentPage} navItems={navItems} />
+        <Navigation
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+          navItems={navItems}
+        />
       </div>
     </div>
   )
